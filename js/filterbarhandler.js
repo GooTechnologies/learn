8c1322e5cba92dc3f4919f74e14fa95d4c4bf6c0
Stefan Hedman <schteppe@gmail.com>
2016-03-10 11:30:41 +0100

    improve filtering and stuff

--- js/filterbarhandler.js
+++ js/filterbarhandler.js
@@ -1,56 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  function getPosts(allposts) {
-      console.log(allposts);
-  };
+/* global $ */
 
-  function filterPosts(allposts) {
-
-  };
-
-  $( ".reset" ).click(function() {
-    $(".overviewblock").hide();
-    $(".overviewblock").fadeIn();
+document.addEventListener("DOMContentLoaded", function() {
+  $(".reset").click(function() {
+    $(".overviewblock").hide().fadeIn();
     $(".difficulty").val($(".difficulty option:first").val());
     $(".usecase").val($(".usecase option:first").val());
     $(".coding").val($(".coding option:first").val());
-});
-
-  $('.difficulty,.usecase,.coding').on('change', function (e) {
-    $(".overviewblock").hide();
-    var selecteddifficulty = $(".difficulty option:selected").text().trim();
-    if (selecteddifficulty == "Select difficulty"){
-      selecteddifficulty = "";
-    }
-    else {
-      selecteddifficulty = "." + selecteddifficulty;
-    }
-
-    var selectedusecase = $(".usecase option:selected").text().trim();
-    if (selectedusecase == "Select usecase"){
-      selectedusecase = "";
-    }
-    else if (selectedusecase == "Other usecases"){
-      selectedusecase = ".Else";
-    }
-    else {
-      selectedusecase = "." + selectedusecase ;
-    }
-
-    var selectedcodeoption = $(".coding option:selected").text().trim();
-    if (selectedcodeoption == "Coding options" || selectedcodeoption == "Both"){
-      selectedcodeoption = "";
-    }
-    if (selectedcodeoption == "Code"){
-      selectedcodeoption = ".code";
-    }
-    else if (selectedcodeoption == "No code"){
-      selectedcodeoption = ".nocode";
-    }
-    var classes = ".overviewblock" + selecteddifficulty + selectedusecase+ selectedcodeoption;
-    $(classes).fadeIn();
   });
 
+  $('.difficulty,.usecase,.coding').on('change', function () {
+    $(".overviewblock").hide();
 
+    var difficultySelector = $(".difficulty option:selected").val();
+    if (difficultySelector !== ''){
+      difficultySelector = '.difficulty' + difficultySelector;
+    }
 
+    var usecaseSelector = $(".usecase option:selected").val();
+    if (usecaseSelector !== ""){
+      usecaseSelector = ".usecase" + usecaseSelector;
+    }
 
+    var codeSelector = $(".coding option:selected").val();
+    if (codeSelector !== ""){
+      codeSelector = ".code" + codeSelector;
+    }
+
+    var filterSelector = ".overviewblock" + difficultySelector + usecaseSelector + codeSelector;
+    $(filterSelector).fadeIn();
+  });
 });
